c1679a6015ae3ca6188c7be0628c00db8820feb7
Swastik Mukherjee <swastik.reach@gmail.com>
2021-02-02 18:49:48 +0545

    Reading data from mongodb atlas and loading it into the page dynamically functionality added

--- server.js
+++ server.js
@@ -17,8 +17,10 @@ mongodb.connect(connectionString, { useNewUrlParser: true }, (err, client) => {
 
 
 app.use(urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  res.send(`<!DOCTYPE html>
+  db.collection('items').find().toArray((err, items) => {
+    res.send(`<!DOCTYPE html>
     <html>
     <head>
       <meta charset="UTF-8">
@@ -40,38 +42,28 @@ app.get('/', (req, res) => {
         </div>
         
         <ul class="list-group pb-5">
-          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">Fake example item #1</span>
-            <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
-            </div>
-          </li>
-          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">Fake example item #2</span>
-            <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
-            </div>
-          </li>
-          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">Fake example item #3</span>
-            <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
-            </div>
-          </li>
+          ${items.map((item) => {
+      return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+      <span class="item-text">${item.text}</span>
+      <div>
+        <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+        <button class="delete-me btn btn-danger btn-sm">Delete</button>
+      </div>
+    </li>`;
+    }).join('')}
         </ul>
         
       </div>
       
     </body>
     </html>`);
+  });
+
 });
 
 app.post('/create-item', (req, res) => {
   db.collection('items').insertOne({ text: req.body.item }, () => {
-    res.send("Thanks!!");
+    res.redirect('/');
   });
 });
 
